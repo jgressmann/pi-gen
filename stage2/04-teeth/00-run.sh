7ea23782112a9794157c068365d15c7934a56e24
Jean Gressmann <jean@0x42.de>
2024-04-21 17:50:55 +0200

    Add initial files for teeth

---
+++ stage2/04-teeth/00-run.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+set -eu
+
+log "TEETH: Install packages required for RPI build"
+# install tools required to build
+apt-get -y update
+apt-get -y install --no-install-recommends \
+	crossbuild-essential-arm64 \
+	cmake
+rm -rf /var/lib/apt/lists/*
+
+# /pi-gen/stage2/04-teeth
+work_dir=$PWD
+
+log "TEETH: Building C parts"
+# C parts
+build_dir=/tmp/teeth/build
+mkdir -p /tmp/teeth/build && cd /tmp/teeth/build
+CC=aarch64-linux-gnu-gcc cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX="${ROOTFS_DIR}/usr/local" "${work_dir}/teeth"
+cmake --build .
+cmake --install .
+cd "${work_dir}"
+rm -rf "${build_dir}"
+
+log "TEETH: Installing service"
+# Python parts
+service_dir=${ROOTFS_DIR}/opt/teeth
+mkdir -p "${service_dir}"
+install -m 644 teeth/service/teeth.py "${service_dir}"
+install -m 644 teeth/service/requirements.txt "${service_dir}"
+install -m 644 files/config.yaml "${service_dir}"
+
+on_chroot << EOF
+	cd /opt/teeth
+	python3 -m venv .
+	. bin/activate
+	pip install -r requirements.txt
+EOF
+
+log "TEETH: System configuration"
+install -m 0644 "files/eth0.network" "${ROOTFS_DIR}/etc/systemd/network/eth0.network"
+install -m 0644 "files/99-teeth.rules" "${ROOTFS_DIR}/etc/udev/rules.d"
+install -m 0755 "files/teethd-start" "${ROOTFS_DIR}/usr/local/bin"
+
+mkdir -p "${ROOTFS_DIR}/usr/local/lib/systemd/system"
+install -m 0644 "files/teeth-device@.service" "${ROOTFS_DIR}/usr/local/lib/systemd/system"
+install -m 0644 "files/teeth-rest.service" "${ROOTFS_DIR}/usr/local/lib/systemd/system"
+
+# don't wait for online (AutoIP)
+sed -i "s/#WAIT_ONLINE_METHOD=ifup/WAIT_ONLINE_METHOD=none/" "${ROOTFS_DIR}/etc/default/networking"
+
+on_chroot << EOF
+	systemctl mask NetworkManager.service
+	systemctl mask ModemManager.service
+	systemctl mask avahi-daemon.service
+	systemctl mask bluetooth.service
+	systemctl mask wpa_supplicant.service
+	systemctl enable systemd-networkd
+	systemctl enable teeth-rest.service
+	systemctl enable ssh.service
+EOF
+
+
+
